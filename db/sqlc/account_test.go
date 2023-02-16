24eaf561b3cf96d04dee4eb6c48a4ae8d72ff29a
LukmanE22 <unsurlukman@gmail.com>
2023-02-16 15:09:57 +0700

    update account_test file

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 	"time"
 
@@ -56,20 +57,44 @@ func TestUpdateAccount(t *testing.T) {
 	
 	updateData := UpdateAccountParams{
 		ID: account_2.ID,
-		Balance: 999,
+		Balance: util.RandomMoney(),
 	}
 	account_2, err = testQueries.UpdateAccount(context.Background(), updateData)
 	
+	require.NotEmpty(t, account_2)
 	require.NoError(t, err)
 	require.Equal(t, account_2.ID, account_1.ID)
 	require.Equal(t, account_2.Owner, account_1.Owner)
 	require.Equal(t, account_2.Balance, updateData.Balance)
 	require.Equal(t, account_2.Currency, account_1.Currency)
-	require.Equal(t, account_2.CreatedAt, account_1.CreatedAt)
+	require.WithinDuration(t, account_2.CreatedAt, account_1.CreatedAt, time.Second)
 }
 
-// func TestAccountsList(t *testing.T) {
-// 	for i := 0; i < 10; i++ {
-// 		CreateRandomAccount(t)
-// 	}
-// }
+func TestDeleteAccount(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
+}
+
+func TestAccountsList(t *testing.T) {
+	for i := 0; i < 20; i++ {
+		CreateRandomAccount(t)
+	}
+
+	args := AccountsListParams{
+		Limit: 5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.AccountsList(context.Background(), args)
+	require.NoError(t, err)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
